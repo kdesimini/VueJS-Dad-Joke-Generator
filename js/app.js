7f9604c6781e20c9edae1098898a24cc7399cc7c
Keith <kdesimini@gmail.com>
2019-10-23 19:27:24 -0400

    Add share button because all dads need these jokes

--- js/app.js
+++ js/app.js
@@ -6,14 +6,17 @@ const balls = new Vue({
         title: 'tap for a dad joke',
         joke: '',
         bgc: {
-			backgroundColor: ''
+            backgroundColor: '',
+            footerColor: ''
 		},
-        colors: ["Aqua", "Aquamarine", "Bisque", "BlanchedAlmond", "Blue", "BlueViolet", "Chartreuse", "Coral", "CornflowerBlue", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSlateBlue", "DarkTurquoise", "DarkViolet", "DeepPink", "DodgerBlue", "FireBrick", "ForestGreen", "Fuchsia", "Gold", "GoldenRod", "Green", "GreenYellow", "HotPink", "IndianRed", "Indigo", "LawnGreen", "LightBlue", "LightCoral", "LightGreen", "LightSeaGreen", "Lime", "LimeGreen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "Navy", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SeaGreen", "Sienna", "SlateBlue", "SlateGray", "SpringGreen", "SteelBlue", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Yellow", "YellowGreen"],
-        show: false
+        colors: ["Aqua", "Aquamarine", "Blue", "BlueViolet", "Chartreuse", "Coral", "CornflowerBlue", "Crimson", "Cyan", "DarkCyan", "DarkGoldenRod", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSlateBlue", "DarkTurquoise", "DarkViolet", "DeepPink", "DodgerBlue", "FireBrick", "ForestGreen", "Fuchsia", "Gold", "GoldenRod", "Green", "HotPink", "IndianRed", "Indigo", "LawnGreen", "Lime", "LimeGreen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "Navy", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SeaGreen", "Sienna", "SlateBlue", "SlateGray", "SpringGreen", "SteelBlue", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Yellow", "YellowGreen"],
+        show: false,
+        shareURL: 'https://icanhazdadjoke.com'
     },
     methods: {
         async showJoke() {
             this.bgc.backgroundColor = this.colors[this.getRandomIntInclusive(0,40)]
+            this.bgc.footerColor = this.colors[this.getRandomIntInclusive(0,40)]
             this.show = false;
             let config = {
                 headers: {
@@ -22,6 +25,9 @@ const balls = new Vue({
             }
             const jokes = await axios.get('https://icanhazdadjoke.com', config);
             this.joke = jokes.data.joke;
+            this.shareURL = 'https://icanhazdadjoke.com';
+            this.shareURL = this.shareURL + '/j/' + jokes.data.id;
+            console.log(this.shareURL);
             this.show = true;
             setTimeout(() => {
                 this.show = true;
